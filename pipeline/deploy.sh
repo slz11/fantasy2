811e87ad084ad96d3a6bdf0a5c546a9bb148c331
slz11 <138655440+slz11@users.noreply.github.com>
2023-07-05 09:52:16 +0000

    shift index

---
+++ pipeline/deploy.sh
@@ -0,0 +1,17 @@
+#! /usr/bin/env sh
+
+set -e
+
+echo "executing $0 {"
+
+rm -rf fantasy2_webserver/
+git clone https://github.com/slz11/fantasy2_webserver.git
+cd fantasy2_webserver/
+cp ../index.html .
+git add --all
+git commit -m "$(date)"
+git push
+
+
+rm -rf fantasy2_webserver/
+echo "done $0}"
