8fd22ff5fcdda65796a058f18218ca03e8e6ad78
slz11 <138655440+slz11@users.noreply.github.com>
2023-07-05 10:12:39 +0000

    add user config

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -7,6 +7,9 @@ echo "executing $0 {"
 rm -rf fantasy2_webserver/
 git clone "https://$MACHINE_USER_TOKEN@github.com/slz11/fantasy2_webserver.git"
 cd fantasy2_webserver/
+git config user.email "user@machine.com"
+git config user.name "Machine User"
+
 cp ../index.html .
 git add --all
 git commit -m "$(date)" || true
