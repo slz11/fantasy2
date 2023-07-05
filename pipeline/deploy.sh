3ef1ce965d272a936e4e1d4a4809bb7458b64efd
slz11 <138655440+slz11@users.noreply.github.com>
2023-07-05 10:08:21 +0000

    machine user to deploy

--- pipeline/deploy.sh
+++ pipeline/deploy.sh
@@ -5,13 +5,13 @@ set -e
 echo "executing $0 {"
 
 rm -rf fantasy2_webserver/
-git clone https://github.com/slz11/fantasy2_webserver.git
+git clone "https://$MACHINE_USER_TOKEN@github.com/slz11/fantasy2_webserver.git"
 cd fantasy2_webserver/
 cp ../index.html .
 git add --all
-git commit -m "$(date)"
+git commit -m "$(date)" || true
 git push
 
-
+cd ../
 rm -rf fantasy2_webserver/
 echo "done $0}"
